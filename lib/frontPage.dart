3ac0f5bb2fad5f18edf80c16a4657e203e7639dd
viniciusbaca <viniciusfbaca@usp.br>
2021-07-09 16:45:01 -0300

    feat: square frontpage

--- lib/frontPage.dart
+++ lib/frontPage.dart
@@ -6,9 +6,36 @@ class FrontPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Alive'),
+      backgroundColor: Color.fromRGBO(40, 45, 56, 1),
+      body: Center(
+        child: Container(
+          color: Color.fromRGBO(227, 169, 107, 1),
+          margin: EdgeInsets.all(40),
+          constraints: BoxConstraints(
+            minWidth: 800,
+            maxWidth: 1500,
+            minHeight: MediaQuery.of(context).size.height * 0.8,
+            maxHeight: MediaQuery.of(context).size.height * 0.9,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Hi, I'm Baca",
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+                Text("A undergrad student at USP, pursuing a Bachelor "
+                    "of Information Systems, currently in the fourth period.\n"
+                    "oi giu")
+              ],
+            ),
+          ),
         ),
       ),
     );
