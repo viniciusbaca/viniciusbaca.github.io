ccd58f86a28b479b50644b2f0febbb1aec4e7c81
viniciusbaca <viniciusfbaca@usp.br>
2021-07-09 12:21:23 -0300

    feat: default FiraCode font

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:personal_website/frontPage.dart';
 
 void main() {
-  runApp(MaterialApp(home: FrontPage()));
+  runApp(MaterialApp(
+      theme: ThemeData(fontFamily: 'FiraCode'),
+      home: FrontPage()));
 }
