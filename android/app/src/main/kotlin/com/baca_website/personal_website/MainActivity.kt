5d938321e66d328a37f563108d983ae90f457d8e
viniciusbaca <53339224+viniciusbaca@users.noreply.github.com>
2021-07-09 23:58:15 +0000

    Deploying to build from @ viniciusbaca/viniciusbaca.github.io@56cce92b344ab03455b615619e00b10d3cb4af55 🚀

--- android/app/src/main/kotlin/com/baca_website/personal_website/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.baca_website.personal_website
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:personal_website/frontPage.dart';
-
-void main() {
-  runApp(MaterialApp(
-    title: 'Baca',
-      theme: ThemeData(fontFamily: 'FiraCode'),
-      home: FrontPage()));
-}
