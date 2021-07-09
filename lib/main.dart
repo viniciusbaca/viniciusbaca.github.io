8bb3de4945b74f54230b3b22cd9fbf0d0d2c4800
viniciusbaca <53339224+viniciusbaca@users.noreply.github.com>
2021-07-09 22:00:00 +0000

    Deploying to main from @ viniciusbaca/viniciusbaca.github.io@fb87cdfeba2f59d64991d489408830f94e69469d 🚀

--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:personal_website/frontPage.dart';
-
-void main() {
-  runApp(MaterialApp(
-      theme: ThemeData(fontFamily: 'FiraCode'),
-      home: FrontPage()));
-}
