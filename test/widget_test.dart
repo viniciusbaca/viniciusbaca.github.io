0976283b00fd85a423db9744e2a1981adc9a0fe2
viniciusbaca <viniciusfbaca@usp.br>
2021-07-09 12:06:57 -0300

    feat: alive

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:personal_website/main.dart';
+import 'package:personal_website/frontPage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(FrontPage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
