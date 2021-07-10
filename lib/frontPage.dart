2feaeee773fd4060f86c5bc819a8c4eaae5d45ec
viniciusbaca <viniciusfbaca@usp.br>
2021-07-09 22:05:56 -0300

    feat: added skills and interests texts

--- lib/frontPage.dart
+++ lib/frontPage.dart
@@ -23,7 +23,9 @@ class FrontPage extends StatelessWidget {
           ),
           Text(
             desc,
-            style: TextStyle(fontSize: greeter ? 20 : 18),
+            style: TextStyle(
+                fontWeight: greeter ? FontWeight.bold : FontWeight.normal,
+                fontSize: greeter ? 20 : 18),
           ),
         ],
       ),
@@ -35,37 +37,68 @@ class FrontPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color.fromRGBO(40, 45, 56, 1),
       body: Center(
-        child: Container(
-          color: Color.fromRGBO(227, 169, 107, 1),
-          margin: EdgeInsets.all(40),
-          constraints: BoxConstraints(
-            minWidth: 800,
-            maxWidth: 1500,
-            minHeight: MediaQuery.of(context).size.height * 0.8,
-          ),
-          child: Column(
-            children: [
-              titleAndDesc(
-                title: "Hi, I'm Baca",
-                desc: "A undergrad student at USP, mainly interested in "
-                    "technology and its applications into business strategy.",
-                greeter: true,
-              ),
-              titleAndDesc(
-                title: "Education",
-                desc: "Pursuing a Bachelor of Information Science at ICMC - "
-                    "USP, the Institute of Mathematics and Computer Science at "
-                    "São Carlos, currently in the fourth period.",
-              ),
-              titleAndDesc(
-                title: "Work",
-                desc: "Currently I'm a Operations Analyst at U-Get, a startup"
-                    " in the computer vision industry. My main activities are"
-                    " managing the operations of 50 new machines to the market,"
-                    " develop a app that's the core of the setup and maintenance"
-                    " of all machines of the company. testando builddsds",
-              ),
-            ],
+        child: SingleChildScrollView(
+          child: Container(
+            color: Color.fromRGBO(227, 169, 107, 1),
+            margin: EdgeInsets.all(40),
+            constraints: BoxConstraints(
+              minWidth: 800,
+              maxWidth: 1500,
+              minHeight: MediaQuery.of(context).size.height * 0.8,
+            ),
+            child: Column(
+              children: [
+                titleAndDesc(
+                  title: "Hi, I'm Baca",
+                  desc: "A undergrad student at USP, mainly interested in "
+                      "technology and its applications into business strategy.",
+                  greeter: true,
+                ),
+                titleAndDesc(
+                  title: "Education",
+                  desc: "Pursuing a Bachelor of Information Science at ICMC - "
+                      "USP, the Institute of Mathematics and Computer Science at "
+                      "São Carlos, currently in the fourth period.",
+                ),
+                titleAndDesc(
+                  title: "Work",
+                  desc: "Currently I'm a Operations Analyst at U-Get, a startup"
+                      " in the computer vision industry. My main activities are"
+                      " managing the operations of 50 new machines to the market,"
+                      " develop a app that's the core of the setup and maintenance"
+                      " of the machines of the company and I also keep the"
+                      " backoffice activities of the company and talent"
+                      " acquisition.",
+                ),
+                titleAndDesc(
+                    title: "Skills",
+                    desc: "My most solid technical skill is in Flutter, which"
+                        " I developed a mobile marketplace (https://hort-e.com/)"
+                        " to connect rural producers and food stores as the product"
+                        " for a startup I was a Co-Founder."
+                        "Right now I'm studying now more in depth algorithms and"
+                        " data structure, also beginning my studies towards"
+                        " data science, learning Python and performing side"
+                        " projects mainly in this ecosystem, such as the Bloomberg"
+                        " Hackhathon, where in a great team we developed a ETF"
+                        " monitor in the Bloomberg Terminal, so the fund managers"
+                        " can gather all the information about any ETF that he "
+                        " will analyze."),
+                titleAndDesc(
+                  title: "Interests",
+                  desc:
+                      "In my laptop I have Arch Linux for daily use and Neovim"
+                      " as my tool for coding (still a beginner).\n"
+                      "In my spare time I enjoy playing with my dog, hang out"
+                      " with Giulia and read books, currently I'm more in "
+                      "technical books for fun, such as 'The Rust Programming "
+                      "Language and 'Fluent Python'.\n"
+                      "I'm genuinely interested in people development, and"
+                      " I'm really grateful for all my friends and colleagues"
+                      " that always gave me support.",
+                ),
+              ],
+            ),
           ),
         ),
       ),
