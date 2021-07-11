70715da2a961ded5aa2b91240e3318786eafe119
viniciusbaca <53339224+viniciusbaca@users.noreply.github.com>
2021-07-11 14:23:03 +0000

    Deploying to build from @ viniciusbaca/viniciusbaca.github.io@2e035bb55f3c25628c17d14f8599ed0c6365d959 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "245671eb2caba275acaacffddc3ae645",
-"index.html": "11df599585ce31b15089133cb784c32d",
-"/": "11df599585ce31b15089133cb784c32d",
+"index.html": "b80c7689b2c6e44d99d2931b2ce2b544",
+"/": "b80c7689b2c6e44d99d2931b2ce2b544",
 "manifest.json": "eff29b7452551f73d8000ec572e8134c",
 "assets/NOTICES": "f58e9534f8bca923859da3a352f53a52",
 "assets/FontManifest.json": "b4ab73f04bd5b097b7e62e40352279e9",
